af0b6b027621834226c08acd418b9bcb055729a6
Nourhan-Nabil-9 <nnorhan535@gmail.com>
2026-07-15 06:19:12 +0300

    enhance : update enviroment (used proxy angular)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,9 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://upskilling-egypt.com:3000/api/v0/',
-  assetUrl: 'https://upskilling-egypt.com:3000/',
+  apiUrl: '/api/v0/',
+  assetUrl: '/',
   appName: 'HMS',
   googleClientId: '155662404542-qeg8enk67uu12h3mcpmfgebojk1sb5ik.apps.googleusercontent.com',
-  stripePublishableKey:'pk_test_51OTjURBQWp069pqTmqhKZHNNd3kMf9TTynJtLJQIJDOSYcGM7xz3DabzCzE7bTxvuYMY0IX96OHBjsysHEKIrwCK006Mu7mKw8',
+  stripePublishableKey:
+    'pk_test_51OTjURBQWp069pqTmqhKZHNNd3kMf9TTynJtLJQIJDOSYcGM7xz3DabzCzE7bTxvuYMY0IX96OHBjsysHEKIrwCK006Mu7mKw8',
 };
